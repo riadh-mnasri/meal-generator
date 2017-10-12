2f81590c7c1b929f505388bc4cbfc29a29dc455a
Riadh MNASRI <riadh.mnasri@gmail.com>
2017-10-12 13:30:40 +0200

    Init App Base

---
+++ src/main/java/com/wehightech/menugenerator/NgMenuGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.wehightech.menugenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgMenuGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgMenuGeneratorApplication.class, args);
+	}
+}
